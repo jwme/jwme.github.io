508951ce728c655785e386805c33d04659c6b2e7
jwme <ocraftproducoes@gmail.com>
2021-02-08 13:49:07 -0300

    Modificado

--- scripts/index.script.js
+++ scripts/index.script.js
@@ -1,10 +1,22 @@
-function criarConta(valor) {
-    if (valor == "abrir") {
+function abrirCriarConta(valor) {
+    if (valor) {
         document.getElementById("criar-conta").style.display = "block";
         document.getElementById("wp").style.display = "block";
     }
-    else if (valor == "fechar") {
+    else {
         document.getElementById("criar-conta").style.display = "none";
         document.getElementById("wp").style.display = "none";
     }
+}
+
+function enviarCriarConta() {
+    let dados = {
+        usuario: document.getElementById("u-cc").value,
+        nome: document.getElementById("n-cc").value,
+        sobrenome: document.getElementById("sb-cc").value,
+        senha: document.getElementById("s-cc").value,
+        confsenha: document.getElementById("cs-cc").value
+    }
+
+    document.getElementById("envio").src = `https://script.google.com/macros/s/AKfycbzv4KtjWQoNZ7IfahFl5WYn1Z4mJhSie5o0_OSQ_GC2pYJf9wLI5xEvhw/exec?usuario=${dados.usuario}&nome=${dados.nome}&sobrenome=${dados.sobrenome}&senha=${dados.senha}`;
 }
