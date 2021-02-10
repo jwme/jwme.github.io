14b4b4a3b1a1c4a18c811300199c97d4330253e2
jwme <ocraftproducoes@gmail.com>
2021-02-10 07:24:41 -0300

    Modificado

--- scripts/index.script.js
+++ scripts/index.script.js
@@ -10,6 +10,8 @@ function abrirCriarConta(valor) {
 }
 
 function enviarCriarConta() {
+    let loader = document.getElementById("loader");
+
     let dados = {
         usuario: document.getElementById("u-cc").value,
         nome: document.getElementById("n-cc").value,
@@ -19,4 +21,40 @@ function enviarCriarConta() {
     }
 
     document.getElementById("envio").src = `https://script.google.com/macros/s/AKfycbwq-Rt7BKCY6fRIOuEn9TLykGJaSd9y2V6jwYXfSz2XzvVQrFjtGwM9Xw/exec?usuario=${dados.usuario}&nome=${dados.nome}&sobrenome=${dados.sobrenome}&senha=${dados.senha}`;
+
+    loader.style.display = "block";
+
+    setTimeout(verificarResposta, 3000)
+}
+
+function verificarResposta() {
+    let loader = document.getElementById("loader");
+
+    const key = "AIzaSyCuQth5jU0v8OIjICZwvduMc8W0pfA2z3c";
+
+    let ajax = new XMLHttpRequest();
+    ajax.open("GET", `https://sheets.googleapis.com/v4/spreadsheets/1kqj6jshF-r1yAuCz3wtUn9dVyY8BEN35Y4oDTw0af6w/values/Cadastro!A2:C1000?key=${key}`)
+
+    ajax.onreadystatechange = () => {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+                let resposta = JSON.parse(ajax.responseText);
+                let valores = resposta.values;
+
+                for (let i = 0; i < valores.length;) {
+                    if (dados.usuario == valores[i][0]) {
+                        if (valores[i][1] == "OK") {
+                            loader.innerHTML = '<h1 style="text-align: center;">OK</<h1>'; 
+                        }
+                        else {
+                            loader.innerHTML = '<h1 style="text-align: center;">Erro</<h1>';
+                        }
+                    }
+                    else {
+                        i++;
+                    }
+                }
+            }
+        }
+
+    ajax.send();      
 }
