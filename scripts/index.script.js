8d639febc4a048cad3bdb2fa16e1b54af0da9bd0
Otávio Oliveira <otaviooliveira0099@gmail.com>
2021-03-12 07:10:44 -0300

    Modificado

---
+++ scripts/index.script.js
@@ -0,0 +1,34 @@
+let indexSlide = 0;
+let margin = 0;
+
+function mudarSlide(direction) {
+    let inner = document.getElementById("inner");
+    let next = document.getElementById("next");
+    let previous = document.getElementById("previous");
+
+    if (direction == "right") {
+        if (margin < 300)
+        {
+            margin += 100;
+            inner.style.marginLeft = `-${margin}%`;
+            previous.style.opacity = 1;
+            previous.disabled = false;
+        }
+        else {
+            next.style.opacity = 0.2;
+            next.disabled = true;           
+        }
+    }
+    else {
+        if (margin > 0) {
+            margin -= 100;
+            inner.style.marginLeft = `-${margin}%`
+            next.style.opacity = 1;
+            next.disabled = false;         
+        }
+        else {
+            previous.style.opacity = 0.2;
+            previous.disabled = true;
+        }
+    }
+    }
